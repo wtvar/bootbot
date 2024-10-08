059bb2aac76777a17b0e3adbe0879d26675fd102
wtvar <wtvariwnt@gmail.com>
2024-10-08 13:28:18 +0100

    finished bookbot

--- main.py
+++ main.py
@@ -14,25 +14,32 @@ def get_word_count(text):
 def get_char_count(text):
     new_dict = {}
     for char in text:
-        if char.lower() in new_dict:
-            new_dict[char.lower()] += 1
+        if char.isalpha():            
+            if char.lower() in new_dict:
+                new_dict[char.lower()] += 1
+            else:
+                new_dict[char.lower()] = 1
         else:
-            new_dict[char.lower()] = 1
+            pass
     return new_dict
 
 def dict_transfer(dict):
-    new_dict = {}
+    
+    big_list = []
     for key,value in dict.items():
+        new_dict = {}
         new_dict["name"] = key
         new_dict["num"] = value
-    return new_dict
+        big_list.append(new_dict)
+    return big_list
 
 def report_print(path,length,chars):
     print(f"--- Begin report of {path} ---")
     print(f'{length} words found in document')
-    for char,count in chars.items():
-        print(f"The '{char}' character was found {count} times")
-
+    print("")
+    for item in chars:
+        print(f"The '{item['name']}' character was found {item['num']} times")
+    print("--- End report ---")
 def sort_on(dict):
     return dict["num"]
 
@@ -44,13 +51,16 @@ def main():
     length = get_word_count(book_text)
     char_count = get_char_count(book_text)
     #print(char_count)
+    #print("..........................")
     """list_of_char_dicts = list(char_count.items())
     list_of_char_dicts.sort(reverse=True, key=sort_on)
     print(list_of_char_dicts)
     report_print(path_to_file,length,list_of_char_dicts)"""
     #report_print(path_to_file,length,char_count)
-    print(dict_transfer(char_count))
-    
+    converted_dicts = dict_transfer(char_count)
+    converted_dicts.sort(reverse=True, key=sort_on)
+    #print(converted_dicts)
+    report_print(path_to_file,length,converted_dicts)
 main()
 
 
